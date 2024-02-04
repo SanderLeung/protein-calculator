2050d53f5a1f82a0b40266cb4a7882b76213907f
Sander Leung <sander.leung@gmail.com>
2024-02-03 22:32:09 -0800

    style tweaks

--- src/react/src/pages/Account.jsx
+++ src/react/src/pages/Account.jsx
@@ -64,42 +64,44 @@ export default function Account({ session }) {
   }
 
   return (
-    <form onSubmit={updateProfile} className="form-widget">
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="text" value={session.user.email} disabled />
-      </div>
-      <div>
-        <label htmlFor="username">Name</label>
-        <input
-          id="username"
-          type="text"
-          required
-          value={username || ''}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="website">Website</label>
-        <input
-          id="website"
-          type="url"
-          value={website || ''}
-          onChange={(e) => setWebsite(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <button className="button block primary" type="submit" disabled={loading}>
-          {loading ? 'Loading ...' : 'Update'}
-        </button>
-      </div>
-
-      <div>
-        <button className="button block" type="button" onClick={() => supabase.auth.signOut()}>
-          Sign Out
-        </button>
-      </div>
-    </form>
+    <div className="min-w-full min-h-screen flex items-center justify-center bg-gray-700">
+      <form onSubmit={updateProfile} className="form-widget">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="text" value={session.user.email} disabled />
+        </div>
+        <div>
+          <label htmlFor="username">Name</label>
+          <input
+            id="username"
+            type="text"
+            required
+            value={username || ''}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="website">Website</label>
+          <input
+            id="website"
+            type="url"
+            value={website || ''}
+            onChange={(e) => setWebsite(e.target.value)}
+          />
+        </div>
+  
+        <div>
+          <button className="w-full primary" type="submit" disabled={loading}>
+            {loading ? 'Loading ...' : 'Update'}
+          </button>
+        </div>
+  
+        <div>
+          <button className="w-full" type="button" onClick={() => supabase.auth.signOut()}>
+            Sign Out
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
