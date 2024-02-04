13a1943e1868e7d0a93c628bcfb52e4d43ce0f8a
Sander Leung <sander.leung@gmail.com>
2024-02-04 01:39:36 -0800

    Add table sorting

--- src/react/src/components/GroceryTable.jsx
+++ src/react/src/components/GroceryTable.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsFillInfoCircleFill } from 'react-icons/bs';
@@ -6,6 +7,7 @@ import { BiSolidDownArrow } from "react-icons/bi";
 import { BiSolidUpArrow } from "react-icons/bi";
 
 // Consider moving these into a utils/calc folder
+const getName = (grocery) => grocery.name;
 const getLeanness = (grocery) => ((grocery.protein / grocery.calories) * 400).toFixed(2)
 const getCostEffectiveness = (grocery) => ((grocery.protein * grocery.servings) / grocery.cost).toFixed(2)
 const getMaxCostEffectiveness = (groceries) => {
@@ -16,9 +18,32 @@ const getMaxCostEffectiveness = (groceries) => {
 
 const GroceryTable = ({ groceries }) => {
   const top = getMaxCostEffectiveness(groceries);
+  const [sortParam, setSortParam] = useState((null));
+  const [order, setOrder] = useState("asc");
 
+  const handleSortingChange = (accessor) => {
+    const handleSorting = (sortParam, sortOrder) => {
+      if (sortParam) {
+        groceries.sort((a, b) => {
+          if (sortParam(a) === null) return 1;
+          if (sortParam(b) === null) return -1;
+          if (sortParam(a) === null && sortParam(b) === null) return 0;
+          return (
+            sortParam(a).toString().localeCompare(sortParam(b).toString(), "en", {
+              numeric: true,
+            }) * (sortOrder === "asc" ? 1 : -1)
+          );
+        });
+      }
+    };
 
-
+    const sortOrder =
+      accessor === sortParam && order === "asc" ? "desc" : "asc";
+    setSortParam(() => accessor);
+    setOrder(sortOrder);
+    handleSorting(accessor, sortOrder);
+  };
+  
   return (
     <table className='w-full border-separate border-spacing-2'>
       <thead>
@@ -26,22 +51,25 @@ const GroceryTable = ({ groceries }) => {
           <th className='w-1/3 border border-slate-600 rounded-md'>
             <div className='flex justify-center items-center gap-x-1'>
               Name
-              <button><BiSolidDownArrow /></button>
-              <button><BiSolidUpArrow /></button>
+              <button onClick={() => handleSortingChange(getName)}>
+                <BiSolidUpArrow /><BiSolidDownArrow />
+              </button>
             </div>
           </th>
           <th className='w-1/4 border border-slate-600 rounded-md'>
             <div className='flex justify-center items-center gap-x-1'>
               Leanness
-              <button><BiSolidDownArrow /></button>
-              <button><BiSolidUpArrow /></button>
+              <button onClick={() => handleSortingChange(getLeanness)}>
+                <BiSolidUpArrow /><BiSolidDownArrow />
+              </button>
             </div>
           </th>
           <th className='w-1/4 border border-slate-600 rounded-md'>
             <div className='flex justify-center items-center gap-x-1'>
               Cost Effectiveness
-              <button><BiSolidDownArrow /></button>
-              <button><BiSolidUpArrow /></button>
+              <button onClick={() => handleSortingChange(getCostEffectiveness)}>
+                <BiSolidUpArrow /><BiSolidDownArrow />
+              </button>
             </div>
           </th>
           <th className='w-1/6 border border-slate-600 rounded-md'>Operations</th>
@@ -66,13 +94,13 @@ const GroceryTable = ({ groceries }) => {
             <td className='border border-slate-700 rounded-md text-center'>
               <div className='flex justify-center items-center gap-x-4'>
                 <Link to={`/groceries/details/${grocery.id}`}>
-                  <BsFillInfoCircleFill className='text-l text-blue-900' />
+                  <BsFillInfoCircleFill className='text-l fill-blue-700' />
                 </Link>
                 <Link to={`/groceries/edit/${grocery.id}`}>
-                  <AiOutlineEdit className='text-2xl text-blue-800' />
+                  <AiOutlineEdit className='text-2xl fill-blue-800' />
                 </Link>
                 <Link to={`/groceries/delete/${grocery.id}`}>
-                  <MdOutlineDelete className='text-xl text-red-600' />
+                  <MdOutlineDelete className='text-xl fill-red-600' />
                 </Link>
               </div>
             </td>
--- src/react/src/components/NavBar.jsx
+++ src/react/src/components/NavBar.jsx
@@ -7,12 +7,12 @@ export function Navbar() {
     <nav className="flex justify-between gap-x-2 px-4 py-2">
       <h1 className="text-title font-bold text-2xl">
         <Link to='/' className='flex justify-between items-center'>
-          <MdHome className='text-sky-800 text-4xl' />
+          <MdHome className='fill-sky-800 text-4xl' />
         </Link>
       </h1>
       <div className="flex flex-row gap-x-2 justify-center items-center">
         <Link to='/account' className='flex justify-between items-center'>
-          <MdAccountCircle className='text-sky-800 text-4xl' />
+          <MdAccountCircle className='fill-sky-800 text-4xl' />
         </Link>
       </div>
     </nav>
